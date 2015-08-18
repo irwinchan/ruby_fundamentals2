33b19f4f85f134cea7d60a0c063c78965df7c286
Irwin <irwinchan@me.com>
2015-08-18 16:58:18 -0400

    Exercise 6. Display list alphabetically.

--- exercise6.rb
+++ exercise6.rb
@@ -16,9 +16,14 @@ output_list (grocery_list)
 puts ""
 puts "You have #{grocery_list.count} items on your list"
 
-#check list for bananas
+# check list for bananas
 puts grocery_list.include?("bananas") ? "You need to pick up bananas" : "You don't need to pick up bananas today"
 
-#output the second item
+# output the second item
 puts "The second item on the list is #{grocery_list[1]}."
+
+# output list alphabetically
+puts ""
+output_list(grocery_list.sort)
+
 
